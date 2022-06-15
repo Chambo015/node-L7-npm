50df0d81675efdff195b96a1b545dcacf26ecbe5
Aydeke <aydeke2@gmail.com>
2022-06-15 22:01:19 +0600

    Add add.ejs

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ fs.readFile(filePath, "utf8", (err, data) => {
 app.set("port", process.env.port || 3000);
 app.engine("ejs", require("ejs-locals"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(app.get("port"), () => {
   console.log(`Сервер слушает порт ${app.get("port")}`);
@@ -27,12 +28,27 @@ app.get("/", (req, res) => {
   res.render("index", { news });
 });
 
-app.get("/:index", (req, res) => {
+app.get("/:index(\\d+)", (req, res) => {
   let { index } = req.params;
   index = Number(index);
   res.render("article", {
     ...news[index],
     prev: index > 0 ? index - 1 : -1,
     next: index < news.length - 1 ? index + 1 : -1,
+  });
+});
+
+app.get("/add", (req, res) => {
+  res.render("add.ejs");
+});
+
+app.post("/add", (req, res) => {
+  const { title, image, text } = req.body;
+  news.push({ title, image, text });
+  json.news = news;
+  fs.writeFile(filePath, JSON.stringify(json), (err) => {
+    if (err) throw err;
+    console.log("Файл записан.");
+    res.redirect(`/${news.length - 1}`);
   });
 });
