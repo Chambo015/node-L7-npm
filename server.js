a169f8ae12c6829b84c948e19642dc2998336e89
Aydeke <aydeke2@gmail.com>
2022-06-15 21:34:43 +0600

    Add article.ejs

--- server.js
+++ server.js
@@ -3,12 +3,32 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 
+let json;
+let news;
+const filePath = path.join(__dirname, "files/db.json");
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) throw err;
+  console.log("Файл новостей прочитан.");
+  json = JSON.parse(data);
+  news = json.news;
+});
+
 app.set("port", process.env.port || 3000);
+app.engine("ejs", require("ejs-locals"));
+app.set("view engine", "ejs");
 
 app.listen(app.get("port"), () => {
   console.log(`Сервер слушает порт ${app.get("port")}`);
 });
 
+app.use("/bootstrap", express.static("./node_modules/bootstrap"));
+
 app.get("/", (req, res) => {
-  res.send("<h1>Привет от Экспресс!</h1>");
+  res.render("index", { news });
+});
+
+app.get("/:index", (req, res) => {
+  let { index } = req.params;
+  index = Number(index);
+  res.render("article", { ...news[index] });
 });
